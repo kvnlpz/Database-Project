d1ba969e567412779ab9c4c0f94c22b6dd12fdf2
molitanuon <43227934+molitanuon@users.noreply.github.com>
2020-05-12 14:37:32 -0700

    Update control.php

--- control.php
+++ control.php
@@ -22,14 +22,15 @@
             //Given the campus wide ID of a student, list all courses the student took and the
             //grades.
             if ($SSN != null) {
-                $result = mysql_query("select * from course, sections, professor where social_security_number =  $SSN");
-                printf("<tr><td>" . "Title" . "</td><td>" . "Classroom" . "</td><td>" . "Days" . "</td><td>" . "start" . "</td><td>" .  "end" . "</td></tr>");
+                  $result = mysql_query("SELECT social_security_number, course_title, class_room, meeting_days, beginning_time, ending_time
+                FROM professor, sections, course
+                WHERE social_security_number = $SSN
+                AND sec_ssn = social_security_number
+                AND course_num = unique_number");
+                printf("<tr><td>" . "Title" . "</td><td>" . "Classroom" . "</td><td>" . "Days" . "</td><td>" . "start" . "</td><td>" .  "end" . "</td></t$
                 while ($row = mysql_fetch_array($result)) {
-                    printf("<tr><td>" . $row['title'] . "</td><td>" . $row['class_room'] . "</td><td>". $row['meeting_days'] . "</td><td>" . $row[beginning_time] . "</td><td>" . $row[ending_time] . "</td></tr>");
+                    printf("<tr><td>" . $row['course_title'] . "</td><td>" . $row['class_room'] . "</td><td>". $row['meeting_days'] . "</td><td>" . $row[$
                 }
-//                while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
-//                    printf("1:%s,    2:%s,    3:%s,    4:%s,    5:%s,    6:%s,    7:%s,    8:%s,    9:%s,    10:%s, \n", $row[1] , $row[2] , $row[3] , $row[4] , $row[5] , $row[6] , $row[7] , $row[8] , $row[9] , $row[10]);
-//                }
 
             } else {
                 $result = mysql_query("SELECT * FROM student WHERE campus_wide_id = $CWID");
